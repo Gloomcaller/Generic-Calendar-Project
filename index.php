a8c80cd1ea533e730735f7c33aeeb83b8f755495
Vojislav Beli <94176223+Gloomcaller@users.noreply.github.com>
2025-07-10 11:00:53 +0200

    Temp HTML done.
    Should be enough to start working on CSS and for initial testing.

---
+++ index.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Calendar</title>
+    <meta name="description" content="Portfolio Calendar Project">
+    <link rel="stylesheet" href="./styles/styles.css">
+</head>
+
+<body>
+    <header>
+        <h1>📅 Calendar<br> Calendar Project</h1>
+    </header>
+
+    <div class="clock-container">
+        <div id="clock"></div>
+    </div>
+
+    <div class="calendar">
+        <div class="nav-btn-container">
+            <button class="nav-btn">⏮️</button>
+            <h2 id="monthYear"></h2>
+            <button class="nav-btn">⏭️</button>
+        </div>
+
+        <div class="calendar-grid" id="calendar"></div>
+    </div>
+
+    <div class="modal" id="eventModal">
+        <div class="modal-content">
+
+
+            <div id="eventSelectorWrapper">
+                <label for="eventSelector">
+                    <strong>Select Event:</strong>
+                </label>
+                <select id="eventSelector">
+                    <option disabled selected>Choose Event...</option>
+                </select>
+            </div>
+
+            <form id="eventForm" method="POST">
+                <input id="formAction" type="hidden" name="action" value="add">
+                <input type="hidden" name="event_id" id="eventId">
+
+                <label for="courseName">Course Title:</label>
+                <input type="text" name="course_name" id="courseName" required>
+
+                <label for="instructorName">Instructor Name:</label>
+                <input type="text" name="instructor_name" id="instructorName" required>
+
+                <label for="startDate">Start Date:</label>
+                <input type="date" name="start_date" id="startDate" required>
+
+                <label for="endDate">End Date:</label>
+                <input type="date" name="end_date" id="endDate" required>
+
+                <button type="submit">💾 Save</button>
+            </form>
+
+            <form action="" method="POST" onsubmit="return confirm('Are you sure you want to delete this event ?')">
+                <input type="hidden" name="action" value="delete">
+                <input type="hidden" name="event_id" id="deleteEventId">
+                <button type="submit" class="submit-btn">🗑️ Delete</button>
+            </form>
+
+            <button type="button" class="submit-btn">❌ Cancel</button>
+        </div>
+    </div>
+    <script src="./scripts/script.js"></script>
+</body>
+
+</html>
